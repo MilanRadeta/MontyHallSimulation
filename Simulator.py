54e34ffab3167d245af0700250e358c8d68bc259
Milan Radeta <milan.radeta.peca@gmail.com>
2020-11-25 20:11:44 +0100

    Use config and score in simulator

--- Simulator.py
+++ Simulator.py
@@ -1,36 +1,33 @@
-import pandas
-from Doors import Doors
+from Score import Score
+from Config import Config
+from DoorsGame import DoorsGame
 
 
 class Simulator(object):
-    def __init__(self, config):
-        self.strategies = config['strategies']
-        self.successDefinitions = config['successDefinitions']
-        self.totalDoors = config['totalDoors']
-        self.totalTries = config['totalTries']
 
-    def simulate(totalTries, totalDoors, choiceStrategy, successDefinitions, log=False):
+    def __init__(self, config):
+        self.config = config
+        self.score = Score(config)
+        self.currentConfig = Config()
+        self.currentConfig.successDefinitions = self.config.successDefinitions
+
+    def simulate(self, log=False):
         if log:
             print(
-                f'Running simulation for totalTries={totalTries}, totalDoors={totalDoors}, choiceStrategy={choiceStrategy}, successDefinitions={successDefinitions}')
-        wins = {}
-        for i in range(totalTries):
-            hasWon = Doors(totalDoors, choiceStrategy, successDefinitions).runGame()
-            for w in hasWon:
-                if w not in wins:
-                    wins[w] = 0
-                wins[w] += 1 if hasWon[w] else 0
-        return wins
+                f'Running simulation for {self.currentConfig}')
+        for i in range(self.currentConfig.totalTries):
+            result = DoorsGame(self.currentConfig).runGame()
+            self.score.update(result, self.currentConfig)
 
     def run(self):
-        for numOfTries in self.totalTries:
-            for numberOfDoors in self.totalDoors:
-                data = {}
-                for choiceStrategy in self.strategies:
-                    data[choiceStrategy.__name__] = {}
-                    wins = Simulator.simulate(numOfTries, numberOfDoors, choiceStrategy, self.successDefinitions)
-                    data[choiceStrategy.__name__] = {definition: f'{round(wins[definition]/numOfTries * 100, 2)}%' for definition in wins}
-                print()
-                print(f'Tries: {numOfTries} | Doors: {numberOfDoors}')
-                print(pandas.DataFrame(data))
-                print()
+        self.score.reset()
+        for totalTries in self.config.totalTries:
+            for totalDoors in self.config.totalDoors:
+                for strategy in self.config.strategy:
+                    self.currentConfig.strategy = strategy
+                    self.currentConfig.totalDoors = totalDoors
+                    self.currentConfig.totalTries = totalTries
+
+                    self.simulate()
+                self.score.printByConfig(self.currentConfig)
+        self.score.print()
