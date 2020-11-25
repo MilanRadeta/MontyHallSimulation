c4fa6542231c77897c381d5e1ef4c9686f83423c
Milan Radeta <milan.radeta.peca@gmail.com>
2020-11-26 00:56:27 +0100

    Use ScoreItems and plot scores

--- Score.py
+++ Score.py
@@ -1,9 +1,12 @@
-from Config import Config
 import pandas
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ScoreItem import ScoreItem
+from Config import Config
 
 
 class Score(object):
-    def __init__(self, config):
+    def __init__(self, config: Config):
         self.config = config
         self.reset()
 
@@ -16,7 +19,7 @@ class Score(object):
                 for strategy in self.config.strategy:
                     scoreForStrategy = {}
                     for definition in self.config.successDefinitions:
-                        scoreForStrategy[definition.__name__] = 0
+                        scoreForStrategy[definition.__name__] = ScoreItem()
                     scoreForDoors[strategy.__name__] = scoreForStrategy
                 scoreForTries[totalDoors] = scoreForDoors
             self.score[totalTries] = scoreForTries
@@ -30,21 +33,37 @@ class Score(object):
             self.incScore(config, definition,
                           1 if score[definition] else 0)
 
-    def printSingleScore(self, scores, totalTries):
+    def processScores(self, scores):
         print('PROBABILITIES')
         data = {}
         for (strategy, definitions) in scores.items():
             data[strategy] = {}
             for (definition, score) in definitions.items():
-                data[strategy][definition] = f'{round(score/totalTries * 100, 2)}%'
-        print(pandas.DataFrame(data))
+                data[strategy][definition] = score.mean()
+        return data
 
-    def printByConfig(self, config):
-        scores = self.score[config.totalTries][config.totalDoors]
+    def printScores(self, scores, config):
         print()
+        print('PROBABILITIES')
         print(f'Tries: {config.totalTries} | Doors: {config.totalDoors}')
-        self.printSingleScore(scores, config.totalTries)
+        print(scores)
         print()
+
+    def plotScores(self, scores, config):
+        sns.heatmap(scores, annot=True)
+        plt.xticks(rotation=0)
+        plt.text(x=1.75, y=-0.3, s='PROBABILITIES',
+                 fontsize=16, weight='bold')
+        plt.text(x=1.8, y=-0.2, s=f'sims={config.totalTries}, doors={config.totalDoors}',
+                 fontsize=10, weight='normal')
+        plt.show()
+
+    def printByConfig(self, config: Config):
+        scores = self.score[config.totalTries][config.totalDoors]
+        scores = self.processScores(scores)
+        scores = pandas.DataFrame(scores)
+        self.printScores(scores, config)
+        self.plotScores(scores, config)
 
     def print(self):
         for (totalTries, doors) in self.score.items():
