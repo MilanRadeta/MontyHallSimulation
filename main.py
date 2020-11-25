7785fb71a20c945b0288d34c7514d06295f6754d
Milan Radeta <milan.radeta.peca@gmail.com>
2020-11-25 20:11:57 +0100

    Call simulator with config

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from Config import Config
 from SuccessDefinition import AllDefinitions
 from Simulator import Simulator
 from ChoiceStrategy import AllStrategies
 
-Simulator({
-    'strategies': AllStrategies,
-    'successDefinitions': AllDefinitions,
-    'totalDoors': (3, 5, 10),
-    'totalTries': [10**(i+1) for i in range(5)],
-}).run()
+config = Config()
+config.strategy = AllStrategies
+config.successDefinitions = AllDefinitions
+config.totalDoors = [3, 5, 10]
+config.totalTries = [10**(i+1) for i in range(5)]
+Simulator(config).run()
