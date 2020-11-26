0e717ac20626c4b9298bab20d3e6fef26e31448a
Milan Radeta <milan.radeta.peca@gmail.com>
2020-11-26 02:15:24 +0100

    Clean up score item

--- ScoreItem.py
+++ ScoreItem.py
@@ -1,5 +1,3 @@
-from numpy.core.numeric import correlate
-from numpy.lib.function_base import cov
 from numpy.core.fromnumeric import mean
 
 
@@ -16,9 +14,3 @@ class ScoreItem(object):
 
     def mean(self):
         return mean(self.values)
-
-    def cov(self, other):
-        return cov(self.values, other.values)
-
-    def correlate(self, other):
-        return 1 - abs(self.mean() - other.mean())
