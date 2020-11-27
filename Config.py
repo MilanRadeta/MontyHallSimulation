855a67f2a7507890469e6bbec9c4c63c2920db29
Milan Radeta <milan.radeta@itengine.rs>
2020-11-27 18:39:57 +0100

    Add opening strategy and init choice strategy to config

--- Config.py
+++ Config.py
@@ -1,10 +1,21 @@
+from ChoiceStrategy import Random, RandomClosed
+
 class Config(object):
 
-    def __init__(self):
+    def __init__(self, config = None):
         self.strategy = None
         self.totalDoors = None
         self.totalTries = None
+
+        self.openingStrategy = RandomClosed
+        self.initChoiceStrategy = Random
         self.successDefinitions = None
+
+        if (config is not None):
+            self.openingStrategy = config.openingStrategy
+            self.initChoiceStrategy = config.initChoiceStrategy
+            self.successDefinitions = config.successDefinitions
+        
 
     def __str__(self):
         return f'<strategy={self.strategy.__name__}, totalDoors={self.totalDoors}, totalTries={self.totalTries}>'
