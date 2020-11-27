4f5c585816145ace4822fd4266643ec07ad3cb44
Milan Radeta <milan.radeta@itengine.rs>
2020-11-27 18:42:03 +0100

    Add scripts for testing init choice and opening strategies

--- initChoice.py
+++ initChoice.py
@@ -1,9 +1,10 @@
 from Config import Config
 from SuccessDefinition import AllDefinitions
 from Simulator import Simulator
-from ChoiceStrategy import CommonStrategies
+from ChoiceStrategy import CommonStrategies, FirstDoor
 
 config = Config()
+config.initChoiceStrategy = FirstDoor
 config.strategy = CommonStrategies
 config.successDefinitions = AllDefinitions
 config.totalDoors = [3, 5, 10]
--- openingStrategies.py
+++ openingStrategies.py
@@ -1,9 +1,10 @@
 from Config import Config
 from SuccessDefinition import AllDefinitions
 from Simulator import Simulator
-from ChoiceStrategy import CommonStrategies
+from ChoiceStrategy import CommonStrategies, LastClosedDoor, PreviouslyChosenDoor
 
 config = Config()
+config.openingStrategy = LastClosedDoor
 config.strategy = CommonStrategies
 config.successDefinitions = AllDefinitions
 config.totalDoors = [3, 5, 10]
