87c43996d3c3ca1389980787f4159020a64655dd
Milan Radeta <milan.radeta.peca@gmail.com>
2020-11-29 04:54:23 +0100

    Get subconfigs from config

--- Config.py
+++ Config.py
@@ -20,5 +20,33 @@ class Config(object):
         return [self.initChoiceStrategy, self.totalTries,
                 self.totalDoors, self.strategy, self.successDefinitions]
 
+    def getPrioritizedPropKeys(self):
+        return ["initChoiceStrategy", "totalTries", "totalDoors", "strategy", "successDefinitions"]
+
+    def getSubconfigs(self, depth):
+        res = [self]
+        propKeys = self.getPrioritizedPropKeys()[:depth]
+
+        for key in propKeys:
+            newSubs = []
+            for sub in res:
+                newSubs.extend(sub.getSubconfigsByKey(key))
+            if (len(newSubs) > 0):
+                res = newSubs
+
+        return res
+
+    def getSubconfigsByKey(self, key):
+        value = self.__dict__[key]
+        res = []
+        if isinstance(value, list):
+            for elem in value:
+                config = Config(self)
+                config.__dict__[key] = elem
+                res.append(config)
+            return res
+
+        return []
+
     def __str__(self):
         return f'<strategy={self.strategy.__name__}, totalDoors={self.totalDoors}, totalTries={self.totalTries}>'
